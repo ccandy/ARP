c7231963bb2b95c82ad47695b1972f1737a216f2
ccandy <ccunandy2001@gmail.com>
2024-12-23 11:29:10 -0800

    Setup Unity project

---
+++ Assets/Scripts/RenderPipeline/Runtime/ARPAsset/ARPAsset.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/ARP Asset")]
+public class ARPAsset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new ARP();
+    }
+}
---
+++ Assets/Scripts/RenderPipeline/Runtime/RenderPipeline/ARP.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+
+public class ARP : RenderPipeline
+{
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        
+    }
+}
