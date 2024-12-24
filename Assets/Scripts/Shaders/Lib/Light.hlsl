0417657f49bacf5cd7f4863c678fedc61ffe2782
ccandy <ccunandy2001@gmail.com>
2024-12-24 12:37:54 -0800

    add Lit Shader

---
+++ Assets/Scripts/Shaders/Lib/Light.hlsl
@@ -0,0 +1,21 @@
+#ifndef ARP_LIGHT
+#define ARP_LIGHT
+
+
+struct Light
+{
+    float3 color;
+    float3 direction;
+};
+
+
+Light GetDirectionalLight()
+{
+    Light light;
+
+    return light;
+    
+}
+
+
+#endif
