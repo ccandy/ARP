213e4665f859ed2c82a3339536cb4548179c2d45
ccandy <ccunandy2001@gmail.com>
2025-02-25 17:25:58 +0800

    calculate world to shadow matrix

--- Assets/Scripts/RenderPipeline/Runtime/Shadows/Shadow.cs
+++ Assets/Scripts/RenderPipeline/Runtime/Shadows/Shadow.cs
@@ -18,6 +18,7 @@ public class Shadow
     private Vector4[] _cullingSpheres = new Vector4[MAX_DIRECTION_SHADOW_COUNT];
     
     
+    
     private CommandBuffer shadowBuffer = new CommandBuffer
     {
         name = BUFFERNAME
@@ -87,7 +88,7 @@ public class Shadow
                 BatchCullingProjectionType.Orthographic
             );
             shadowDrawSetting.splitData = shadowSplitData;
-            SetTileViewport(index * cascadeCount  + n , split, tileSize);
+            Vector2 offset = SetTileViewport(index * cascadeCount  + n , split, tileSize);
             shadowBuffer.SetViewProjectionMatrices(viewMat,projMat);
             RPUtil.ExecuteBuffer(ref _context, shadowBuffer);
             _context.DrawShadows(ref shadowDrawSetting);
@@ -98,9 +99,13 @@ public class Shadow
                 cullingSphere.w *= cullingSphere.w;
                 _cullingSpheres[n] = cullingSphere;
             }
+            Matrix4x4 projViewMat = projMat * viewMat;
+            Matrix4x4 worldToShadowMat = ConvertMatrix(projViewMat, split, offset);
             
-            
+            _dirViewProjectionMatrices[index * cascadeCount + n] = worldToShadowMat;
         }
+        
+        SendToGPU();
     }
     
     private void RenderDirectionalShadows(ref CullingResults cullingResults)
@@ -130,9 +135,35 @@ public class Shadow
         
     }
 
-    private void ConvertMatrix(ref Matrix4x4 viewMat, ref Matrix4x4 projMat)
+    private Matrix4x4 ConvertMatrix(Matrix4x4 m, int split, Vector2 offset)
     {
+        if (SystemInfo.usesReversedZBuffer)
+        {
+            m.m20 = -m.m20;
+            m.m21 = -m.m21;
+            m.m22 = -m.m22;
+            m.m23 = -m.m23;
+        }
         
+        float scale = 1.0f / split;
+        
+        //x
+        m.m00 = (0.5f * (m.m00 + m.m30) + offset.x * m.m30) * scale;
+        m.m01 = (0.5f * (m.m01 + m.m31) + offset.x * m.m31) * scale;
+        m.m02 = (0.5f * (m.m02 + m.m32) + offset.x * m.m32) * scale;
+        m.m03 = (0.5f * (m.m03 + m.m33) + offset.x * m.m33) * scale;;
+        //y
+        m.m10 = (0.5f * (m.m10 + m.m30) + offset.y * m.m30) * scale;
+        m.m11 = (0.5f * (m.m11 + m.m31) + offset.y * m.m31) * scale; 
+        m.m12 = (0.5f * (m.m12 + m.m32) + offset.y * m.m32) * scale;; 
+        m.m13 = (0.5f * (m.m13 + m.m33) + offset.y * m.m33) * scale;; 
+        //z
+        m.m20 = 0.5f * (m.m20 + m.m30);
+        m.m21 = 0.5f * (m.m21 + m.m31);
+        m.m22 = 0.5f * (m.m22 + m.m32);
+        m.m23 = 0.5f * (m.m23 + m.m33);
+        
+        return m;
     }
     
     public void CleanUP()
@@ -151,11 +182,13 @@ public class Shadow
         
     }
     
-    void SetTileViewport(int index, int split,int tileSize)
+    private Vector2 SetTileViewport(int index, int split,int tileSize)
     {
         Vector2 offset = new Vector2(index % split, index / split);
         shadowBuffer.SetViewport(new Rect(offset.x * tileSize, offset.y * tileSize, tileSize, tileSize));
         RPUtil.ExecuteBuffer(ref _context, shadowBuffer);
+
+        return offset;
     }
     
     
